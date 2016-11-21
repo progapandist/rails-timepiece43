2ea730d7cb0c892ce11188c6d749fdbf11f87675
Andy B <andybarnov@gmail.com>
2016-11-21 15:44:11 +0100

    add validations to models

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,3 +1,6 @@
 class Watch < ApplicationRecord
   belongs_to :user
+  validates :description, presence: true
+  validates :name, presence: true
+  validates :price, presence: true, numericality: true
 end
