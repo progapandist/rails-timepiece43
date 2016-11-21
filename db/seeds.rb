fab7f66434c824feab53324392e38cf521d30983
Andy B <andybarnov@gmail.com>
2016-11-21 17:56:27 +0100

    add has_attachment to Watch model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  user = User.create!(email: Faker::Internet.email,
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              password: '123456')
+end
