15378eda354c3dc4d402a69953e43f06d62206ac
Hugo Revel <hugorevel@gmail.com>
2016-11-21 17:41:26 +0100

    Update on Index method for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,10 @@
 class BookingsController < ApplicationController
 
   def index
-    @bookings = Booking.all
+    @user = User.find(params[:user_id])
+    watches_array = @user.watches.map do |watch|
+      watch.id
+    end
+    @bookings = Booking.where(watches_array.include?(watch_id))
   end
 end
