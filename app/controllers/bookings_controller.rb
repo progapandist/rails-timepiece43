10442fe8bf0bcfb110dd2e1075ba62a06ceb4f1a
Hugo Revel <hugorevel@gmail.com>
2016-11-22 15:52:00 +0100

    Able to create and show a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,17 @@
 class BookingsController < ApplicationController
 
   def index
-    @user = User.find(params[:user_id])
-    watches_array = @user.watches.map do |watch|
-      watch.id
-    end
-    @bookings = Booking.where(watches_array.include?(:watch_id))
+    @user = current_user
+    @bookings = Booking.where(user: @user)
   end
 
   def show
-    if current_user.id == params[:user_id]
-      @booking = Booking.find(params[:booking_id])
-    else
-      redirect_to new_user_session_path
-    end
+   @booking = Booking.find(params[:id])
   end
 
   def new
+    @watch = Watch.find(params[:watch_id])
     @booking = Booking.new
-    @user = current_user
   end
 
   def create
