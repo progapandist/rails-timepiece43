a15f67538a695700a0eb2dbfb68cd1808d64084c
Andy B <andybarnov@gmail.com>
2016-11-22 19:25:03 +0100

    squash avatar bug

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def user_avatar_or_stub(user)
     if user.avatar?
-      avatar =  user.avatar.path    
-    elsif current_user.facebook_picture_url?
-      avatar = current_user.facebook_picture_url
+      avatar =  user.avatar.path
+    elsif user.facebook_picture_url?
+      avatar = user.facebook_picture_url
     else
       avatar =  "zwlbzgaokjnprpwm9puw"
     end
