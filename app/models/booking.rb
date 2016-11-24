1695cd20ef7727ba93eb965fb6783035842b2b17
Maxime Charvet <maximecharvet@hotmail.com>
2016-11-24 15:58:46 +0100

    mailer completed except picture

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :watch
   belongs_to :user
+
+  after_create :send_pending_email
+
+
+private
+
+  def send_pending_email
+    BookingMailer.pending(self).deliver_now
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-   after_create :send_welcome_email
+  after_create :send_welcome_email
 
 
 
