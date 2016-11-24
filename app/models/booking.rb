ae39d08ac73d80af2c285a774ac2d008c782a1e6
Remi Duvoux <remi.duvoux@gmail.com>
2016-11-24 14:51:24 +0100

    halfway to hell

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :watch
   belongs_to :user
+
+  def accepted?
+    status == "accepted"
+  end
+
+  def pending?
+    status == "pending"
+  end
+
+  def declined?
+    status == "declined"
+  end
 end
